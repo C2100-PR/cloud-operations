4d6a00845090715d0944657c8d0af58acdd8b439
Phillip Corey Roark <pr@coaching2100.com>
2025-01-19 08:32:29 -0600

    Fix WSGI configuration

--- lucy/app.py
+++ lucy/app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return 'Dr Lucy OK'
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', '8080'))
+    app.run(host='0.0.0.0', port=port)
