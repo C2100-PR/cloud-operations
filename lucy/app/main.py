0f68f76717fb1e90ac7c2f9af45f6f0e858a3493
Phillip Corey Roark <pr@coaching2100.com>
2025-01-19 08:24:44 -0600

    Add secret manager integration

--- lucy/app/main.py
+++ lucy/app/main.py
@@ -1,7 +1,19 @@
+import os
 from flask import Flask
+from google.cloud import secretmanager
 
 app = Flask(__name__)
+
+def get_secret(secret_id):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/api-for-warp-drive/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode()
 
 @app.route('/')
 def home():
     return 'Dr. Lucy is operational'
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
